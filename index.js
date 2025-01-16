c35e394e863e836d4b5a2116c62f983f976f80ff
miguelgarbo <miguelggarbo@gmail.com>
2025-01-16 17:00:32 -0300

    Versão 1.0

---
+++ index.js
@@ -0,0 +1,37 @@
+
+
+function removeTransition(e){
+
+if(e.propertyName !== "transform") return;
+
+    console.log(e.propertyName);
+
+    this.classList.remove("playing")
+
+}
+
+window.addEventListener("keydown", function(e){
+
+const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`);
+
+const key = this.document.querySelector(`div[data-key="${e.keyCode}"]`)
+
+  console.log(audio);
+  if(!audio) return;
+
+  audio.currentTime = 0;
+  audio.play();
+
+  key.classList.add("playing")
+  
+
+
+})
+
+const keys = document.querySelectorAll(".key");
+
+keys.forEach(key =>{
+
+    key.addEventListener("transitionend", removeTransition)
+
+});
